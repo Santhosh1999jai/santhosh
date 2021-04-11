f6b268ceeb3241022f0f275f3ef0416efd9e2cf9
ELCOT <ELCOT@Lenovo>
2021-04-10 23:55:56 -0700

    ft

---
+++ Basics/src/Array/Ab.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class Ab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = { 7, 3, 4, 1, 5 };
+		int b[] = { 15, 11, 3, 19, 1 };
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < b.length; j++) {
+				if (a[i] == b[j])
+
+				{
+					System.out.println(a[i]);
+				}
+			}
+		}
+	}
+}
