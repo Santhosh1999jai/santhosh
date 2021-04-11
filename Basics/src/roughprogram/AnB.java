deefce580549eab69f0c71e6c62001c04822f5f7
ELCOT <ELCOT@Lenovo>
2021-04-10 23:55:21 -0700

    santhosh

---
+++ Basics/src/roughprogram/AnB.java
@@ -0,0 +1,20 @@
+package roughprogram;
+
+public class AnB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]={3,4,6,7,9};
+int b[]={67,4,1,6,10,7};
+for(int i=0;i<a.length;i++)
+{
+	for(int j=0;j<b.length;j++)
+	{
+		if(a[i]==b[j])
+		{
+			System.out.println(a[i]);
+		}
+	}
+}
+	}
+}
